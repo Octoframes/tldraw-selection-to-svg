0c6c234be14879a138004a9438844957a30644c5
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-11-20 07:36:33 +0100

    add download button

--- src/App.jsx
+++ src/App.jsx
@@ -49,8 +49,16 @@ export default function App() {
         />
       </div>
 
-      {/* Third Column with Black Border */}
-      <div style={{ width: 500, height: 500, marginLeft: 20, border: '2px solid black' }}>
+      {/* Third Column with Black Border and Download Button */}
+      <div
+        style={{
+          width: 500,
+          height: 500,
+          marginLeft: 20,
+          border: "2px solid black",
+        }}
+      >
+        <DownloadButton data={snapshotData} />
         <div style={{ width: "100%", height: "100%", overflow: "auto" }}>
           {snapshotData && (
             <div dangerouslySetInnerHTML={{ __html: snapshotData }} />
@@ -60,7 +68,6 @@ export default function App() {
     </div>
   );
 }
-
 
 function SaveButton({ onSave }) {
   const editor = useEditor();
@@ -85,5 +92,34 @@ function SaveButton({ onSave }) {
     >
       Export SVG
     </button>
+  );
+}
+
+function DownloadButton({ data }) {
+  const fileName = "exported.svg";
+
+  // Create a Blob from the SVG data
+  const svgBlob = new Blob([data], { type: "image/svg+xml;charset=utf-8" });
+
+  // Generate a URL for the Blob
+  const url = URL.createObjectURL(svgBlob);
+
+  return (
+    <a
+      href={url}
+      download={fileName}
+      style={{
+        position: "absolute",
+        zIndex: 1000,
+        right: 10,
+        top: 10,
+        backgroundColor: "lightblue",
+        padding: "5px 10px",
+        textDecoration: "none",
+        color: "black",
+      }}
+    >
+      Download SVG
+    </a>
   );
 }
